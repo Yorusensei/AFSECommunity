59b2db5b3f35674d16332508d6371bbd480b675b
nnotwen <112250334+nnotwen@users.noreply.github.com>
2026-02-06 01:07:57 +0800

    (feat): added tooltip to buttons + replaced calculator button label to icons

--- src/tabs/calculator/entry.ts
+++ src/tabs/calculator/entry.ts
@@ -5,6 +5,7 @@ import tabNpckills from "./tab-npckills";
 import tabYen from "./tab-yen";
 import { generateUniqueId } from "../../utils/idGenerator";
 import tabChikara from "./tab-chikara";
+import { Tooltip } from "bootstrap";
 
 export default {
 	render(id: string) {
@@ -36,13 +37,52 @@ export default {
 
 		$(selector).prepend(/*html*/ `
 			<div data-calc-navigation="true" class="tab-bar rounded-3 justify-center" data-bs-theme="dark">
-				<button id="${statsCalcTabBtnId}" class="cyber-tab active">STAT</button>
-				<button id="${incCalcTabBtnId}" class="cyber-tab cyber-tab-purple">INC</button>
-				<button id="${npckillsCalcTabBtnId}" class="cyber-tab cyber-tab-red">NPC</button>
-				<button id="${yenCalcTabBtnId}" class="cyber-tab cyber-tab-pink">YEN</button>
-				<button id="${chikaraCalcTabBtnId}" class="cyber-tab cyber-tab-yellow">CHIKARA</button>
+				<button id="${statsCalcTabBtnId}" class="cyber-tab active"><i class="bi bi-bar-chart"></i></button>
+				<button id="${incCalcTabBtnId}" class="cyber-tab cyber-tab-purple"><i class="bi bi-diagram-3"></i></button>
+				<button id="${npckillsCalcTabBtnId}" class="cyber-tab cyber-tab-red"><bi class="bi bi-crosshair"></bi></button>
+				<button id="${yenCalcTabBtnId}" class="cyber-tab cyber-tab-pink"><i class="bi bi-currency-yen"></i></button>
+				<button id="${chikaraCalcTabBtnId}" class="cyber-tab cyber-tab-yellow"><i class="bi bi-gem"></i></button>
 			</div>
 		`);
+
+		new Tooltip(`#${statsCalcTabBtnId}`, {
+			animation: true,
+			title: "STATS CALCULATOR",
+			customClass: "bg-dark-2 text-glow-blue bg-opacity-90 border-cyber border-opacity-30 rounded text-terminal tracking-widest",
+			placement: "bottom",
+		});
+
+		new Tooltip(`#${incCalcTabBtnId}`, {
+			animation: true,
+			title: "INCREMENTAL CALCULATOR",
+			customClass: "bg-dark-2 text-glow-purple bg-opacity-90 border-cyber border-cyber-purple border-opacity-30 rounded text-terminal tracking-widest",
+			placement: "bottom",
+			trigger: "hover",
+		});
+
+		new Tooltip(`#${npckillsCalcTabBtnId}`, {
+			animation: true,
+			title: "NPC KILLS FARMING CALCULATOR",
+			customClass: "bg-dark-2 text-glow-red bg-opacity-90 border-cyber border-cyber-red border-opacity-30 rounded text-terminal tracking-widest",
+			placement: "bottom",
+			trigger: "hover",
+		});
+
+		new Tooltip(`#${yenCalcTabBtnId}`, {
+			animation: true,
+			title: "YEN CALCULATOR",
+			customClass: "bg-dark-2 text-glow-pink bg-opacity-90 border-cyber border-cyber-pink border-opacity-30 rounded text-terminal tracking-widest",
+			placement: "bottom",
+			trigger: "hover",
+		});
+
+		new Tooltip(`#${chikaraCalcTabBtnId}`, {
+			animation: true,
+			title: "CHIKARA CALCULATOR",
+			customClass: "bg-dark-2 text-glow-yellow bg-opacity-90 border-cyber border-cyber-yellow border-opacity-30 rounded text-terminal tracking-widest",
+			placement: "bottom",
+			trigger: "hover",
+		});
 
 		$([statsCalcTabBtnId, incCalcTabBtnId, npckillsCalcTabBtnId, yenCalcTabBtnId, chikaraCalcTabBtnId].map((x) => `#${x}`).join(", ")).on(
 			"click",
